20269468ca4cbc600d8b89a3787a01fddb37b048
orestuk <orko.yarko@gmail.com>
2026-08-13 12:11:35 +0300

    add scripts

---
+++ src/js/menu.js
@@ -0,0 +1,11 @@
+/*const burger = document.getElementById('burger');
+const menu = document.getElementById('mobile-menu');
+const closeBtn = document.getElementById('menu-close');
+
+burger.addEventListener('click', () => {
+  menu.classList.add('is-open');
+});
+
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('is-open');
+});*/
