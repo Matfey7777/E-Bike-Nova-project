b14847ad2a2bdf12fdd4749775f9bb82bad7e2e9
orestuk <orko.yarko@gmail.com>
2026-08-17 19:19:00 +0300

    adds close menu mobile on link click

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,13 @@
 const burger = document.querySelector('.burger-btn');
 const menu = document.getElementById('mobile');
 const closeBtn = document.querySelector('.close-btn');
+const menuLinks = menu.querySelectorAll('.mobile-list-nav-link');
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('is-open');
+  });
+});
 
 burger.addEventListener('click', () => {
   menu.classList.add('is-open');
